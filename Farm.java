7e030715c01008b2689ddd335630e00d053b0bd9
Kelvin Chan <31221043+chan34kelvin@users.noreply.github.com>
2017-11-03 10:34:05 -0700

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -1,4 +1,12 @@
-class Farm 
-{     
-   //your code here
-}
+class Farm  {     
+   private Animal[] aBunchOfAnimals = new Animal[3];    
+   public Farm()     {       
+      aBunchOfAnimals[0] = new Cow("cow","moo");           
+      aBunchOfAnimals[1] = new Chick("chick","gok");       
+      aBunchOfAnimals[2] = new Pig("pig","bork");    }         
+   public void animalSounds()    {       
+     for (int nI=0; nI < aBunchOfAnimals.length; nI++)       {          
+       System.out.println( aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());       
+      }    
+    } 
+} 
